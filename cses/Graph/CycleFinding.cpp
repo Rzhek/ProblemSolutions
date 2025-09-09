c696f7e8264efecb77a9cfbd62fe4b86e3f7c892
Rzhek <vladkurgankin2004@gmail.com>
2025-09-09 01:34:56 -0400

    1am cycle detection

--- cses/Graph/CycleFinding.cpp
+++ cses/Graph/CycleFinding.cpp
@@ -1,36 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x.size())
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
 using ll = long long;
-using pll = pair<ll, ll>;
-const int inf = 1e18;
+
+const ll inf = LLONG_MAX;
+struct Ed { int a, b, w, s() { return a < b ? a : -a; }};
+struct Node { ll dist = inf; int prev = -1; };
+
+void bellmanFord(vector<Node>& nodes, vector<Ed>& eds, int s) {
+    nodes[s].dist = 0;
+    sort(all(eds), [](Ed a, Ed b) { return a.s() < b.s(); });
+
+    int lim = sz(nodes) / 2 + 2;
+    rep(i, 0, lim) for (Ed ed : eds) {
+        Node cur = nodes[ed.a], &dest = nodes[ed.b];
+        if (abs(cur.dist) == inf) continue;
+        ll d = cur.dist + ed.w;
+        if (d < dest.dist) {
+            dest.prev = ed.a;
+            dest.dist = (i < lim-1 ? d : -inf);
+        }
+    }
+    rep(i, 0, lim) for (Ed e : eds) {
+        if (nodes[e.a].dist == -inf)
+            nodes[e.b].dist = -inf;
+    } 
+}
+
 
 int main() {
-    // cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
     int n, m; cin >> n >> m;
-    vector<vector<pll>> edges(n);
-    for (int i = 0; i < n; i++) {
-        ll u, v, w; cin >> u >> v >> w;
+    vector<Node> nodes(n + 1);
+    vector<Ed> eds; eds.reserve(n + m);
+
+    while (m--) {
+        int u, v, w; cin >> u >> v >> w;
         u--; v--;
-        edges[u].emplace_back(v, w);
+        eds.push_back({u, v, w});
     }
+    for (int i = 0; i < n; i++) eds.push_back({n, i, 0});
 
-    vector<ll> dist(n, inf), changed(n, 0);
-    dist[0] = 0;
-    for (int i = 0; i <= n; i++) {
 
-        for (int u = 0; u < n; u++) {
-            if (dist[u] == inf) continue;
-            for (int [v, w] : edges[u]) {
-                if (dist[u] + w < dist[v]) {
-                    // update
-                    dist[v] = dist[u] + w;
-                }
+    bellmanFord(nodes, eds, n);
+
+    // for (int i = 0; i <= n; i++) {
+    //     auto node = nodes[i];
+    //     cout << i + 1 << ": " << node.dist << " " << node.prev + 1 << "\n";
+    // }
+
+    for (int i = 0; i < n; i++) {
+        vector<int> global_vis(n+1);
+        if (!global_vis[i] && nodes[i].dist == -inf && nodes[i].prev != n) {
+            vector<int> res = {i};
+            vector<int> vis(n + 1);
+            int cur = i;
+            while (cur != -1 && !vis[cur]) {
+                // cout << cur << "\n";
+                res.push_back(cur);
+                vis[cur] = 1;
+                global_vis[cur] = 1;
+                cur = nodes[cur].prev;
             }
+            if (cur == -1) continue;
+            cout << "YES\n";
+            res.push_back(cur);
+            reverse(res.begin(), res.end());
+            for (int j = 0; j < res.size(); j++) {
+                cout << res[j] + 1 << " ";
+                if (j && res[j] == res[0]) break;
+            }
+            cout << "\n";
+            return 0;
         }
-
-
     }
 
-    return 0;
+    cout << "NO\n";
 }
