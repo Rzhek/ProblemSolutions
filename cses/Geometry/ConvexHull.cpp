1f152ad03b2d0d0fa7954fa8d338fa38930a9484
Rzhek <vladkurgankin2004@gmail.com>
2025-09-10 00:40:43 -0400

    conv hull fixed

--- cses/Geometry/ConvexHull.cpp
+++ cses/Geometry/ConvexHull.cpp
@@ -71,7 +71,7 @@ typedef Point<ll> P;
 vector<P> convexHull(vector<P> pts) {
     if (sz(pts) <= 1) return pts;
     sort(all(pts));
-    vector<P> h(sz(pts) + 10);
+    vector<P> h(3 * sz(pts) + 5);
     int s = 0, t = 0;
     for (int it = 2; it--; s = --t, reverse(all(pts)))
         for (P p : pts) {
